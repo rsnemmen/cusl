7365b1df8937382a9f6fea88a254b155f2d240a4
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-06-03 10:06:08 -0300

    pasted source from gsl sphere.c, trying to keep notation as much as possible

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ CUSL: A CUDA port of the GNU Scientific Library
 =================================================
 
 Port of some routines of the GNU Scientific Library (GSL) to CUDA.
+
+gsl_ran_dir_3d
+
+gsl_sf_bessel_Kn
+
+gsl_ran_chisq
---
+++ sphere.cpp
@@ -0,0 +1,27 @@
+void
+gsl_ran_dir_3d (const gsl_rng * r, double *x, double *y, double *z)
+{
+  double s, a;
+
+  /* This is a variant of the algorithm for computing a random point
+   * on the unit sphere; the algorithm is suggested in Knuth, v2,
+   * 3rd ed, p136; and attributed to Robert E Knop, CACM, 13 (1970),
+   * 326.
+   */
+
+  /* Begin with the polar method for getting x,y inside a unit circle
+   */
+  do
+    {
+      *x = -1 + 2 * gsl_rng_uniform (r);
+      *y = -1 + 2 * gsl_rng_uniform (r);
+      s = (*x) * (*x) + (*y) * (*y);
+    }
+  while (s > 1.0);
+
+  *z = -1 + 2 * s;              /* z uniformly distributed from -1 to 1 */
+  a = 2 * sqrt (1 - s);         /* factor to adjust x,y so that x^2+y^2
+                                 * is equal to 1-z^2 */
+  *x *= a;
+  *y *= a;
+}
