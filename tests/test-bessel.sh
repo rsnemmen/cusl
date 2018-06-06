dacf6af27dba8204b896117bb60b50e09615a1ce
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-06-06 11:20:06 -0300

    renamed cuda source file

--- tests/test-bessel.sh
+++ tests/test-bessel.sh
@@ -1,13 +1,22 @@
+#!/bin/sh
+#
+# This script compiles and executes the testing units for the GSL and CUDA
+# Bessel function implementations. After running this, you should have
+# two files in the current folder: 
+# 	• `bessel-gsl.dat`, output from GSL routines
+# 	• `bessel-cuda.dat`, output from the CUDA routines
+# 
+
 # compilation
 gcc test-gsl_bessel.c -o test-gsl_bessel -lgsl -lm -lgslcblas
 echo "Compiled GSL code"
-nvcc test-cu_bessel_Kn.cu -o test-cu_bessel_Kn -I../src
+nvcc test-cu_bessel.cu -o test-cu_bessel -I../src
 echo "Compiled CUDA code"
 echo
 
 # tests
-./test-gsl_bessel_Kn 
-./test-cu_bessel_Kn
+./test-gsl_bessel
+./test-cu_bessel
 
 # plot
 python plot-bessel.py
